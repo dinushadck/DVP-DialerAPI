c8068d7dcf5cc87c499ac6a36b4971fc73bf87b3
dinushadck <dinusha@duosoftware.com>
2019-03-07 18:15:46 +0530

    add dialer realtime data sender

---
+++ DuoDialer/DialerRealtimeDataSender.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+
+func AddCampaignDataRealtime(callServerHost string) string {
+	
+}
