40fbc26b600d5eb0e28b1ea85535810cf1c56dbe
dinushadck <dinusha@duosoftware.com>
2019-03-08 13:22:05 +0530

    create map arr

--- DuoDialer/DialerRealtimeDataSender.go
+++ DuoDialer/DialerRealtimeDataSender.go
@@ -1,27 +1,27 @@
 package main
 
-/* import (
+import (
 	"strconv"
 	"fmt"
 	"github.com/fatih/color"
-) */
+)
 
 
 func AddCampaignDataRealtime(campaignData Campaign) {
-	/* color.Cyan(fmt.Sprintf("Adding Campaign"))
-	var campInfoRealTime map[string]string
+	color.Cyan(fmt.Sprintf("Adding Campaign"))
+	campInfoRealTime := make(map[string]string)
 
 	campInfoRealTime["CampaignId"] = strconv.Itoa(campaignData.CampaignId)
 	campInfoRealTime["CampaignName"] = campaignData.CampaignName
 	campInfoRealTime["StartTime"] = campaignData.CampConfigurations.StartDate.Format("02 Jan 06 15:04 -0700")
 	campInfoRealTime["EndTime"] = campaignData.CampConfigurations.EndDate.Format("02 Jan 06 15:04 -0700")
 
-	key := fmt.Sprintf("RealTimeCampaign:%d:%d:%s", campaignData.TenantId, campaignData.CompanyId, campaignData.CampaignId)
+	key := fmt.Sprintf("RealTimeCampaign:%d:%d:%d", campaignData.TenantId, campaignData.CompanyId, campaignData.CampaignId)
 
 	RedisHMSet(key, campInfoRealTime)
 
 	color.Cyan("==============START=============");
 	color.Cyan(fmt.Sprintf("%v", campInfoRealTime));
-	color.Cyan("==============DONE============="); */
+	color.Cyan("==============DONE=============");
 	
 }
