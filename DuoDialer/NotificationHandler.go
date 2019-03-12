5d803b31a591771f3612bd7b985a355181b848d5
dinushadck <dinusha@duosoftware.com>
2019-03-12 14:22:36 +0530

    add notification handler

---
+++ DuoDialer/NotificationHandler.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/fatih/color"
+)
+
+func SendNotificationToRoom(roomName, from, direction, message, ref string, company, tenant int) {
+	defer func() {
+		if r := recover(); r != nil {
+			color.Red(fmt.Sprintf("Recovered in UpdateCampaignStartStatus %+v", r))
+		}
+	}()
+	pushD := PushData{}
+	pushD.From = from
+	pushD.Direction = direction
+	pushD.Message = message
+	pushD.Ref = ref
+
+	jsonData, _ := json.Marshal(pushD)
+
+	jwtToken := fmt.Sprintf("Bearer %s", accessToken)
+	internalAuthToken := fmt.Sprintf("%d:%d", tenant, company)
+	serviceurl := fmt.Sprintf("http://%s/DVP/API/1.0.0.0/NotificationService/Notification/initiate/%s",  CreateHost(notificationServiceHost, notificationServicePort), roomName)
+	req, err := http.NewRequest("POST", serviceurl, bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("authorization", jwtToken)
+	req.Header.Set("companyinfo", internalAuthToken)
+	DialerLog(fmt.Sprintf("request:%s", serviceurl))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer resp.Body.Close()
+
+	DialerLog(fmt.Sprintf("response Status:%s", resp.Status))
+	DialerLog(fmt.Sprintf("response Headers:%s", resp.Header))
+	body, errb := ioutil.ReadAll(resp.Body)
+	if errb != nil {
+		color.Red(err.Error())
+	} else {
+		result := string(body)
+		DialerLog(fmt.Sprintf("response Body:%s", result))
+	}
+}
